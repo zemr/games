9e49f6abdcf24ba647542b28eeff50318852416e
zemr <rzedzicka@gmail.com>
2017-07-04 18:21:57 +0200

    Add gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,55 @@
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
     cleanCSS = require('gulp-clean-css'),
+    autoprefixer = require('gulp-autoprefixer'),
     concat = require('gulp-concat'),
     uglify = require('gulp-uglify'),
-    rename = require('gulp-rename');
+    rename = require('gulp-rename'),
+    sourcemaps = require('gulp-sourcemaps'),
+    browserSync = require('browser-sync').create();
 
 gulp.task('styles', function() {
   gulp.src('src/scss/styles.scss')
-    .pipe(sass({outputStyle: 'expanded'}))
+    .pipe(sass({ outputStyle: 'expanded' }))
+    .pipe(autoprefixer())
     .pipe(gulp.dest('css'))
 });
 
 gulp.task('minify-css', function() {
   gulp.src('css/styles.css')
-    .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(rename({suffix: '.min'}))
+    .pipe(cleanCSS({ compatibility: 'ie8' }))
+    .pipe(rename({ suffix: '.min' }))
     .pipe(gulp.dest('css'))
+    .pipe(browserSync.reload({ stream: true }))
 });
 
 gulp.task('main-script', function() {
   gulp.src('src/js/main.js')
+    .pipe(sourcemaps.init())
     .pipe(uglify())
+    .pipe(sourcemaps.write('../maps'))
     .pipe(gulp.dest('js'))
+    .pipe(browserSync.reload({ stream: true }))
 });
 
 gulp.task('scripts', function() {
   gulp.src('src/js/game-*.js')
+    .pipe(sourcemaps.init())
     .pipe(concat('games.js'))
     .pipe(uglify())
+    .pipe(sourcemaps.write('../maps'))
     .pipe(gulp.dest('js'))
+    .pipe(browserSync.reload({ stream: true }))
 });
 
-gulp.task('watch', function() {
+gulp.task('browser-sync', function() {
+  browserSync.init({
+    server: { baseDir: './' },
+    browser: "google-chrome"
+  })
+});
+
+gulp.task('watch', ['browser-sync'], function() {
   gulp.watch('src/scss/**/*.scss', ['styles']);
   gulp.watch('css/styles.css', ['minify-css']);
   gulp.watch('src/js/main.js', ['main-script']);
